f170f564109694489c4e02f6689d78c6f11566e1
lvang5 <38891207+lvang5@users.noreply.github.com>
2018-08-01 19:44:02 -0500

    updated client.js file with comments

--- client.js
+++ client.js
@@ -1,13 +1,13 @@
 console.log('js');
 
 $(document).ready(readyNow);
-let counter = 1;
+let counter = 1; //global counter
 
 function readyNow() {
     console.log('JQ');
-    $('.btn').click(appendDiv);
-    $('#container').on('click', '.swap', changeColor);
-    $('#container').on('click', '.delete', removeParent);
+    $('.btn').on('click', appendDiv); // when button is clicked run appendDiv function
+    $('#container').on('click', '.swap', changeColor); // look for class container when swap is clicked run changeColor function
+    $('#container').on('click', '.delete', removeParent); // look for class container when delete is clicked run removeParent function
 
 }
 
@@ -16,13 +16,16 @@ function appendDiv() {
     $('#container').append('<div class="generate">' + counter + '<p></p><button class="swap">Swap</button><button class="delete">Delete</button></div>');
     counter++;
     
-
+    //look for class container and append div, paragraph, and two buttons
+    //increment variable counter
 
 
 }
 function changeColor() {
     $(this).parent().toggleClass('highlight');
+    //toggle this parent's class based on the highlight class's styling
 }
 function removeParent() {   
     $(this).parent().remove();
+    // remove this parent's element and everything within it
 }
