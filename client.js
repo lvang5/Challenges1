8e36938e425d33bd2b37002e18a0f0b8f6288b96
lvang5 <38891207+lvang5@users.noreply.github.com>
2018-08-01 19:37:19 -0500

    Add files via upload

--- client.js
+++ client.js
@@ -1,24 +1,28 @@
 console.log('js');
 
 $(document).ready(readyNow);
-let counter = 0;
+let counter = 1;
+
 function readyNow() {
-    console.log('Jquery is now working');
+    console.log('JQ');
     $('.btn').click(appendDiv);
-    
+    $('#container').on('click', '.swap', changeColor);
+    $('#container').on('click', '.delete', removeParent);
+
 }
 
 function appendDiv() {
-    
-    $('body').append('<div class="generate"><p></p><button class="swap">Swap</button><button class="delete">Delete</button></div>');
+
+    $('#container').append('<div class="generate">' + counter + '<p></p><button class="swap">Swap</button><button class="delete">Delete</button></div>');
     counter++;
-    console.log('button was clicked ' + counter + ' time');
-    function changeColor() {
-        $(this).parent().toggleClass('highlight');
-    }
-    function removeParent() {
-        $(this).parent().remove(); 
-    }
-    $('.swap').on( 'click', changeColor);
-    $('.delete').on( 'click', removeParent);
+    
+
+
+
+}
+function changeColor() {
+    $(this).parent().toggleClass('highlight');
+}
+function removeParent() {   
+    $(this).parent().remove();
 }
